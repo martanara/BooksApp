a3e08007283f20d130907721e0eb82c5eee098ca
Marta Nara <marta.kos1230@gmail.com>
2021-11-11 12:13:16 +0100

    Add 'initActions' function

--- src/js/script.js
+++ src/js/script.js
@@ -1,14 +1,20 @@
 {
   'use strict';
 
-  
   const select = {
     templateOf: {
       book: '#template-book',
     },
-    listOf: {
+    containerOf: {
       books: '.books-list',
-    }
+    },
+    bookItem: {
+      clickable: '.books-list .book__image',
+    },
+  };
+
+  const classNames = {
+    classFavorite: 'favorite',
   };
 
   const templates = {
@@ -21,12 +27,36 @@
       const bookData = dataSource.books[book];
       const generatedHTML = templates.bookItem(bookData);
       const booksDOM = utils.createDOMFromHTML(generatedHTML);
-      const booksList = document.querySelector(select.listOf.books);
+      const booksList = document.querySelector(select.containerOf.books);
       booksList.appendChild(booksDOM);
     }
   };
 
-  
+  const favoriteBooks = [];
+
+  const initActions = function(){
+    const bookItems = document.querySelectorAll(select.bookItem.clickable);
+
+    for(let bookItem of bookItems){
+
+      bookItem.addEventListener('dblclick', function(event){
+        event.preventDefault();
+        const bookId = bookItem.getAttribute('data-id');
+        if(!bookItem.classList.contains('favorite')){
+          bookItem.classList.add('favorite');
+          favoriteBooks.push(bookId);
+        } else {
+          bookItem.classList.remove(classNames.classFavorite);
+          const bookIndexOf = favoriteBooks.indexOf(bookItem);
+          favoriteBooks.splice(bookIndexOf, 1);
+        }
+      });
+      bookItem.addEventListener('click', function (event){
+        event.preventDefault();
+      });
+    }
+  };
 
   render();
+  initActions();
 }
